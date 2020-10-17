2534e3598e305a742dd3a38d96db20b77a97eed4
DrMooose <72785566+DrMooose@users.noreply.github.com>
2020-10-17 02:08:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="../styling/home_style.css">
-        <link rel="stylesheet" type="text/css" href="top_style.css">
+        <link rel="stylesheet" type="text/css" href="../components/top_style.css">
     </head>
     <body>
         <div class="main">
