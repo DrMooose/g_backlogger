eb2e0f1ac39ade41488eab1849d57ca36f933cd6
DrMooose <72785566+DrMooose@users.noreply.github.com>
2020-10-30 05:01:48 -0500

    Add files via upload

--- pages/about.php
+++ pages/about.php
@@ -7,7 +7,7 @@
         <?php include "../components/top.html" ?>
         
         <div class="content">
-            <p>MyGameLog is a </p>
+            <p>MyGameLog is a website where users can access a list of games, view information for selected titles, and add any game to their own tracking list. The goal is for users to be able to log into their personal account, see what games they have on their playing, plan to play, completed, or dropped list and be able to easily find any games they may want to add to these list.</p>
         </div>
 
         <?php include "../components/foot.html" ?>
